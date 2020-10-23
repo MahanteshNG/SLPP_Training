db948f74259014774fcc87cefded2211b6b8b459
kaushiknatua <kaushiknatua@yahoo.in>
2020-10-23 18:18:00 +0530

    C# DN004 23rd Oct 2020

---
+++ DN004/C#/DN004/20thOct/AbstractClass_InterfaceExample/AbstractClass_InterfaceExample/Coach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_InterfaceExample
+{
+    public abstract class Coach
+    {
+        public int CoachId { get; set; }
+        public string CoachName { get; set; }
+
+        public Coach()
+        {
+
+        }
+
+        protected Coach(int coachId, string coachName)
+        {
+            CoachId = coachId;
+            CoachName = coachName;
+        }
+
+        public abstract void TraineeAttendance();
+    }
+}
