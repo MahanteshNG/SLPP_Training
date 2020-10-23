424887c95f82df81023ae131c3625af9a8b7a144
kaushiknatua <kaushiknatua@yahoo.in>
2020-10-23 18:11:10 +0530

    C# DN001 23rd Oct 2020

---
+++ DN001/DN001_App3/CollectionsEx1/CollectionsEx1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+//Namepace for Generic Collection Class
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            List<int> PinCodeList = new List<int>();
+            Console.WriteLine("Inital Size of List: " + PinCodeList.Count);
+            string listInput = string.Empty;
+            Console.WriteLine("\nEnter List Data:");
+            do
+            {
+                PinCodeList.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Do you want to enter again(Enter yes):");
+                listInput = Console.ReadLine();
+            }
+            while (listInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase));
+            Console.WriteLine("\nSize of List after Data Addition: " + PinCodeList.Count);
+            Console.WriteLine("List Output:");
+            foreach(var data in PinCodeList)
+            {
+                Console.WriteLine(data);
+            }
+
+            //Set
+            SortedSet<string> CountrySet = new SortedSet<string>();
+            Console.WriteLine("\nEnter data to Set:");
+            string setInput = string.Empty;
+            do
+            {
+                CountrySet.Add(Console.ReadLine());
+                Console.WriteLine("Do you want to enter again(Enter yes):");
+                setInput = Console.ReadLine();
+            }
+            while (setInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase));
+            Console.WriteLine("\nDisplay Set Data:");
+            foreach(var data in CountrySet)
+            {
+                Console.WriteLine(data);
+            }
+            */
+            //Dictionary
+            SortedDictionary<int, string> EmployeeDictionary = new SortedDictionary<int, string>();
+            string dictionaryInput = string.Empty;
+            Console.WriteLine("\nEnter Dictionary Data:");
+            do
+            {
+                EmployeeDictionary.Add(int.Parse(Console.ReadLine()), Console.ReadLine());
+                Console.WriteLine("Do you want to enter again(Enter yes):");
+                dictionaryInput = Console.ReadLine();
+            }
+            while (dictionaryInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase));
+            //-20 or any other values with give the equivalent space between 2 data
+            Console.WriteLine("\nDictionary Output:");
+            Console.WriteLine("{0,-20}{1}", "Employee Id", "Employee Name");
+            foreach(var data in EmployeeDictionary)
+            {
+                Console.WriteLine("{0,-20}{1}", data.Key, data.Value);
+            }
+        }
+    }
+}
---
+++ DN001/DN001_App3/LinqWithCollections/LinqWithCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>()
+            {
+                10,1,15,17,2,14,12,11,9
+            };
+            //Max() and Min() Linq Methods with Collection helps to find the largest
+            //and smallest number present in collection
+            Console.WriteLine("Greatest Number: " + list.Max());
+            Console.WriteLine("\nSmallest Number: " + list.Min());
+
+            //Filter and display all even numbers present in the collection in descending manner
+            //Linq Query
+            var evenNos = (from data in list
+                           where data % 2 == 0
+                           orderby data descending
+                           select data).ToList();
+            //Console.WriteLine("\nevenNos variable datatype: " + evenNos.GetType());
+            Console.WriteLine("\nEven No Display:");
+            foreach(var x in evenNos)
+            {
+                Console.WriteLine(x);
+            }
+            //Lamda Expression
+            Console.WriteLine("\nDisplay Odd Numbers in descending manner:");
+            var oddNos = list.Where(data => data % 2 != 0).OrderByDescending(data => data)
+                            .Select(data => data).ToList();
+            foreach(var x in oddNos)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
