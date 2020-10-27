813a5335bdaa7fac47592e1f68cf5fb20608b4ef
kaushiknatua <kaushiknatua@yahoo.in>
2020-10-27 16:25:07 +0530

    DN004 C# 27th Oct

---
+++ DN004/C#/DN004/20thOct/InheritanceExample/InheritanceExample/Coach.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public class Coach
+    {
+        //any class member,properties or methods declared as prootected scope in parent class
+        //can only be accessible by child class
+        protected int CoachId { get; set; }
+        protected string CoachName { get; set; }
+        public Coach()
+        {
+
+        }
+
+        public Coach(int coachId, string coachName)
+        {
+            CoachId = coachId;
+            CoachName = coachName;
+        }
+        /*
+        public void DisplayCoach()
+        {
+            Console.WriteLine("Coach Id: " + CoachId + "\nCoach Name: " + CoachName);
+        }*/
+        public override string ToString()
+        {
+            return string.Format("\nCoach Name: {0}\nCoach Id: {1}", CoachName, CoachId);
+        }
+    }
+}
---
+++ DN004/C#/DN004/27thOct/FileHandlingEx1/FileHandlingEx1/EmployeeDL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingEx1
+{
+    //storage class
+    public class EmployeeDL
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public double Salary { get; set; }
+        public EmployeeDL()
+        {
+
+        }
+
+        public EmployeeDL(int employeeId, string employeeName, DateTime dateOfJoining, double salary)
+        {
+            EmployeeId = employeeId;
+            EmployeeName = employeeName;
+            DateOfJoining = dateOfJoining;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,-20}{1,-20}{2,-20}{3}",
+                this.EmployeeId, this.EmployeeName, this.DateOfJoining.ToString("dd.MM.yyyy"),
+                this.Salary);
+        }
+    }
+}
